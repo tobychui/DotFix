6826bc50e80ebb8cfc33c1b110ff1936269eea6f
Toby Chui <tobychui@users.noreply.github.com>
2017-02-03 18:19:54 +0800

    Add files via upload
    DotFix Stable Build v1.1

---
+++ Dotfix/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Integer) As Integer
+    Dim result As Integer
+    Dim p() As Process
+    Private Function CheckIfRunning()
+        p = Process.GetProcessesByName("Dotfix")
+        If p.Count > 1 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If CheckIfRunning() = True Then
+            MsgBox("Another Dotfix has been running.")
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Me.Visible = True Then
+            Me.Hide()
+        End If
+        For i = 1 To 255
+            result = 0
+            result = GetAsyncKeyState(i)
+            If result = -32767 Then
+                If (Control.ModifierKeys And Keys.Shift) = False Then
+                    If i = 190 Then
+                        SendKeys.Send(".")
+                    End If
+                End If
+
+            End If
+        Next i
+    End Sub
+End Class
---
+++ Dotfix/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Dotfix")>
+<Assembly: AssemblyDescription("Fix your broken dot button")>
+<Assembly: AssemblyCompany("IMUS Laboratory")>
+<Assembly: AssemblyProduct("Dotfix")>
+<Assembly: AssemblyCopyright("Copyright © IMUS Laboratory 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("58ce740c-1872-4250-9441-3fd59bd85880")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
